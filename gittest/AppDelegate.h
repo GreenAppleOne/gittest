b17723287f1101744c2921a40ca6bf708797d3f6
GreenAppleOne <yuanweigang365@gmail.com>
2013-05-25 20:30:23 +0800

    Initial Commit

---
+++ gittest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by yuanweigang on 13-5-25.
+//  Copyright (c) 2013年 yuanweigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
